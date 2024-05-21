848abc292acd3fa3160db1d7dc7790d89ed81017
user <aqusay06@gmail.com>
2024-05-21 19:40:42 +0300

    Delete css file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/scss/now-ui-dashboard.scss?v1.5.0";
-import "./assets/css/demo.css";
+
 
 import AdminLayout from "./layouts/Admin.js";
 
