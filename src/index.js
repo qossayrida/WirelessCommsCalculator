1c6dea65a23a1fd30185cb4195296709f4bb9b20
user <aqusay06@gmail.com>
2024-05-21 19:17:33 +0300

    Add basename to url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import AdminLayout from "./layouts/Admin.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/WirelessCommsCalculator">
         <Routes>
             <Route path="/admin/*" element={<AdminLayout />} />
             <Route path="*" element={<Navigate to="/admin/home-page" replace />} />
