ed841e2db92919dfd587cf8a91ee62f738a2461b
user <aqusay06@gmail.com>
2024-05-22 12:23:16 +0300

    Edit scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "./assets/scss/dashboard.scss";
+import "./assets/styles/dashboard.scss";
 
 
 import AdminLayout from "./dashboard/Admin.js";
